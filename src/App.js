93ca414f27b15633d2354acf0d1e9d265fc4aa3f
AmyXandra <amyabafor013@gmail.com>
2021-05-08 12:11:24 +0100

    adding files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import '@progress/kendo-theme-default/dist/all.css';
+// import Login from './components/auth/Login';
+import Landing from './components/Landing';
+  // Set the variables here.
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Login/> */}
+      <Landing/>
     </div>
   );
 }
---
+++ src/components/Landing.js
@@ -0,0 +1,81 @@
+import React from 'react';
+
+
+export default function Landing() {
+    return(
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <div>
+                            <h1>The best place to create an Online Quiz That’s Fun To Use</h1>
+                            <p>CodePen is a social development environment for front-end designers and developers. Build and deploy a website, show off your work, build test cases to learn and debug, and find inspiration.</p>
+                            <a className="btn" href="/">Create Quiz</a>
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                    
+                    </div>
+                </div>
+            </div>
+
+            <section>
+                <div className="container">
+                    <div>
+                        <h3>Features  Of Quiz Marker</h3>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-4">
+                            <div>
+                                <img src="" alt="" />
+                                <h4>Create Quiz</h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pharetra ornare at velit sodales. Cras donec tortor varius ipsum nibh sed.</p>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div>
+                                <img src="" alt="" />
+                                <h4>Share Questions with Friends</h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pharetra ornare at velit sodales. Cras donec tortor varius ipsum nibh sed.</p>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div>
+                                <img src="" alt="" />
+                                <h4>Publish</h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pharetra ornare at velit sodales. Cras donec tortor varius ipsum nibh sed.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6"></div>
+                        <div class="col-md-5 offset-md-1">
+                            <div>
+                                <h3>Why Would You Make  A Quiz?</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. At massa fringilla tellus nec. Ut mollis curabitur egestas diam scelerisque quis. Et sed sed vel ut sagittis cursus felis quis. Convallis est volutpat non sed vitae est mauris eget.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            
+            <section>
+                <div className="container">
+                    <div className="text-center">
+                        <h3>Published Quiz</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas facilisis cursus semper quis mattis. </p>
+                    </div>
+                    <div className="row">
+                        
+                    </div>
+                </div>
+            </section>
+            
+        </div>
+    )
+}
